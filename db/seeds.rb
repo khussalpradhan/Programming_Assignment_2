4754b46f5762c083bdaef5e19f78d8a2a30fc7e7
khussalpradhan <pradhankhu@gmail.com>
2025-09-24 00:35:04 -0500

    added movies

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,7 +19,14 @@ more_movies = [
   {:title => 'Nomadland', :rating => 'R',
     :release_date => '19-Feb-2021'},
   {:title => 'CODA', :rating => 'PG-13',
-    :release_date => '13-Aug-2021'}
+    :release_date => '13-Aug-2021'},
+  {:title => 'American Pie', :rating => 'R',
+    :release_date => '09-July-1999'},
+  {:title => 'A Quiet Place Part II', :rating => 'PG-13',
+    :release_date => '28-May-2021'},
+  {:title => 'Bodyguard Of Lies', :rating => 'R',
+    :release_date => '08-Jun-2025'}
+]
 ]
 
 more_movies.each do |movie|
